74c53bee712356076695c8805763765b2bab40c6
kwanulee <kwlee@hansung.ac.kr>
2017-04-01 13:55:44 +0900

    Polymorphism (UpDownCasting) 모듈 코멘트 추

--- Polymorphism/AnimalSimulator(UpDownCasting)/src/com/kwanwoo/java/AnimalSimulator.java
+++ Polymorphism/AnimalSimulator(UpDownCasting)/src/com/kwanwoo/java/AnimalSimulator.java
@@ -1,13 +1,25 @@
 package com.kwanwoo.java;
 
 /**
- * Created by kwanwoo on 2017-03-23.
+ * com.kwanwoo.java.AnimalSimulator
+ * 3개의 com.kwanwoo.java.Dog 객체, 3개의 com.kwanwoo.java.Cat 객체
+ * 3개의 com.kwanwoo.java.Duck 객체를 생성하여 이들을 모두 Animal 객체 배열에 저장한다.
+ *
+ * Animal 객체 배열에서 랜덤으로 선택된 com.kwanwoo.java.Animal 객체의 상태를 출력하는
+ * 과정을 열번 반복하는 간단한 시뮬레이터 프로그램이다.
+ *
+ * 이 버전은 기존 버전 (https://github.com/kwanulee/JavaExample/blob/master/ClassInheritance/AnimalSimulator(Inheritance)/src/com/kwanwoo/java/AnimalSimulator.java)
+ * 에 비해, Dog, Cat, Duck 객체들을 위한 배열을 따로 관리하는 대신에 Dog, Cat, Duck의 수퍼클래스인 Animal 타입의
+ * 객체 배열에 Dog, Cat, Duck 객체들을 저장하고 사용한다.
+ *
+ * Animal 객체배열에 저장된 객체가 어떠한 타입의 객체인지를 알기 위해서 instanceof 연산자를 사용했고
+ * 해당 객체의 고유한 메소드를 이용하기 위해서 각 타입별로 다운캐스팅하여 사용하였다
  */
 public class AnimalSimulator {
     public static void main(String[] args) {
         final int MAX_ANIMALS=9;
 
-        Animal[] animals = new Animal[MAX_ANIMALS];
+        Animal[] animals = new Animal[MAX_ANIMALS];     // Animal 타입의 객체 배열 생성
 
         animals[0] = new Dog("진도개", "화랑", 3);
         animals[1] = new Dog("삽살개","레오",2);
@@ -21,7 +33,7 @@ public class AnimalSimulator {
         animals[7] = new Duck("청둥오리", "하늘",2);
         animals[8] = new Duck("원앙", "잉꼬",2);
 
-        for (int i=0; i<9; i++) {
+        for (int i=0; i<10; i++) {
 
             int choice = (int) (Math.random() * 9);
             Animal animal = animals[choice];
@@ -31,12 +43,12 @@ public class AnimalSimulator {
             int ation = (int) (Math.random() * 5) + 1;
             switch (ation) {
                 case 1:
-                    if (animal instanceof Dog)
-                        ((Dog)animal).bark();
-                    else if (animal instanceof Cat)
-                        ((Cat)animal).meow();
-                    else if (animal instanceof Duck)
-                        ((Duck)animal).quack();
+                    if (animal instanceof Dog)          // animal이 Dog 객체인지 판별
+                        ((Dog)animal).bark();           // animal을 Dog 타입으로 다운캐스팅
+                    else if (animal instanceof Cat)     // animal이 Cat 객체인지 판별
+                        ((Cat)animal).meow();           // animal을 Cat 타입으로 다운캐스팅
+                    else if (animal instanceof Duck)    // animal이 Duck 객체인지 판별
+                        ((Duck)animal).quack();         // animal을 Duck 타입으로 다운캐스팅
                     break;
                 case 2:
                     animal.eat();
@@ -48,14 +60,12 @@ public class AnimalSimulator {
                     animal.move();
                     break;
                 case 5:
-                    if (animal instanceof Duck)
-                        ((Duck)animal).swim();
+                    if (animal instanceof Duck)         // animal이 Duck 객체인지 판별
+                        ((Duck)animal).swim();          // animal을 Duck 타입으로 다운캐스팅
                     else
                         animal.move();
                     break;
             }
-
-
         }
     }
 }
