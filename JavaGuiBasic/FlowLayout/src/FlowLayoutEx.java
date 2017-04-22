273d75a5d8fb5fad3eb78beec593e08a502634aa
kwanulee <kwlee@hansung.ac.kr>
2017-04-23 08:22:21 +0900

    JavaGUIBasic JPanel 컨테이너로 분리

--- JavaGuiBasic/FlowLayout/src/FlowLayoutEx.java
+++ JavaGuiBasic/FlowLayout/src/FlowLayoutEx.java
@@ -5,16 +5,7 @@ public class FlowLayoutEx extends JFrame {
     FlowLayoutEx() {
         setTitle("FlowLayout 예제");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container contentPane = getContentPane(); // 컨텐트팬 알아내기
-
-        // 왼쪽 정렬로, 수평 간격을 30, 수직 간격을 40 픽셀로 배치하는
-        // FlowLayout 생성
-        contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
-        contentPane.add(new JButton("add"));
-        contentPane.add(new JButton("sub"));
-        contentPane.add(new JButton("mul"));
-        contentPane.add(new JButton("div"));
-        contentPane.add(new JButton("Calculate"));
+        setContentPane(new FlowLayoutPanel());
 
         setSize(300, 200); // 프레임 크기 300x200 설정
         setVisible(true); // 화면에 프레임 출력
@@ -22,5 +13,18 @@ public class FlowLayoutEx extends JFrame {
 
     public static void main(String[] args) {
         new FlowLayoutEx();
+    }
+}
+
+class FlowLayoutPanel extends JPanel {
+    FlowLayoutPanel() {
+        // 왼쪽 정렬로, 수평 간격을 30, 수직 간격을 40 픽셀로 배치하는
+        // FlowLayout 생성
+        setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+        add(new JButton("add"));
+        add(new JButton("sub"));
+        add(new JButton("mul"));
+        add(new JButton("div"));
+        add(new JButton("Calculate"));
     }
 }
--- JavaGuiBasic/GridLayout/src/GridLayoutEx.java
+++ JavaGuiBasic/GridLayout/src/GridLayoutEx.java
@@ -5,20 +5,24 @@ public class GridLayoutEx extends JFrame {
     public GridLayoutEx() {
         super("GridLayout 예제");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container contentPane = getContentPane();
-
-        // 1x10의 GridLayout 배치관리자
-        contentPane.setLayout(new GridLayout(1, 10));
-
-        for(int i=0; i<10; i++) { // 10개의 버튼 부착
-            String text = Integer.toString(i); // i를 문자열로 변환
-            JButton button = new JButton(text);
-            contentPane.add(button); // 컨텐트팬에 버튼 부착
-        }
+        setContentPane(new GridLayoutPanel());
         setSize(500, 200);
         setVisible(true);
     }
     public static void main(String[] args) {
         new GridLayoutEx();
+    }
+}
+
+class GridLayoutPanel extends JPanel {
+    GridLayoutPanel() {
+        // 1x10의 GridLayout 배치관리자
+        setLayout(new GridLayout(1, 10));
+
+        for(int i=0; i<10; i++) { // 10개의 버튼 부착
+            String text = Integer.toString(i); // i를 문자열로 변환
+            JButton button = new JButton(text);
+            add(button); // 컨텐트팬에 버튼 부착
+        }
     }
 }
