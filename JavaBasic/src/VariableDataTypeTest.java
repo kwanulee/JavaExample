e384565700c9be14e42c6d4db77fe2da2388c072
kwanulee <kwlee@hansung.ac.kr>
2020-08-15 17:06:19 +0900

    update code

--- JavaBasic/src/VariableDataTypeTest.java
+++ JavaBasic/src/VariableDataTypeTest.java
@@ -1,29 +1,22 @@
 
-
 /**
  * VaraibleDataTypeTest의 main() 함수에는 다수의 오류를 포함한 코드가 있습니다.
  * 변수의 타입과 초깃값의 타입이 맞지 않는 오류가 있고, 상수에 대한 변경의 오류가 있습니다
  *
- * 다음 코드의 main 함수 내의 주석문 을 제거하고, 이 코드가 컴파일 될 수 있도록 수정해 보세요.
+ * 이 코드가 컴파일 될 수 있도록 수정해 보세요.
  */
 public class VariableDataTypeTest {
     public static void main(String args[]) {
-/*
-        boolean a = 10>0;
-        boolean b = 1;          // 타입 오류
-        byte c =  128;          // 타입 오류
-        short d = 129;
-        int e = 300000;
-        long f = 3.14;          // 타입 오류
-        float g = 3.14;         // 타입 오류
-        double h = 3.14;
-        char i = 'a';
-        char j = '한';
-        final double PI = 3.142592;
-
-        System.out.println("c="+c);
-        PI = 3.14;             // 상수 값 변경 오류 (삭제)
-        System.out.println("PI="+PI);
-*/
+    	boolean a = 10 > 0;
+		boolean b = 1;
+		byte c = 128;
+		short d = 129;
+		int e = 3000000000;
+		long f = 3.14;
+		float g = 3.14;
+		double h = 3.14;
+		char i = 'a';
+		char j = '한';
+		char k = "한";
     }
 }
