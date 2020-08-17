8d1a3d1088d1bd0eec473a91d4686023c8a25350
kwanulee <kwlee@hansung.ac.kr>
2020-08-17 23:00:39 +0900

    예제 추가

---
+++ JavaClass/GuiFigureTest-OOP2/src/com/kwanwoo/java/Ameba.java
@@ -0,0 +1,14 @@
+package com.kwanwoo.java;
+
+public class Ameba {
+	final String shape = "AMEBA";
+	final String sound = "ameba.hif";
+	
+	void rotate() {
+		System.out.println("도형의 한쪽 끝을 기준으로 360도 회전:"+shape);
+	}
+	
+	void playSound() {
+		System.out.println("HIF 사운드 재생:"+sound);
+	}
+}
